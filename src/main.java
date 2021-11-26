bec5779e98c6bb7e8c9d3a03c8e94fddf94e823d
vugar350 <Vugar350@mail.ru>
2021-11-26 21:08:25 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String[] args) {
+        int refill = 2000;
+        int bonus=refill/100;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = 100 + refill + bonus;
+        System.out.println(balance);
+        System.out.println(bonus);
+    }
+}
